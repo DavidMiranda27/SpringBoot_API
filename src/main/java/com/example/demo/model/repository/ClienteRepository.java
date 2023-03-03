a7b2c86b55c3722b284bc2df7d6868ee49263f5f
DavidMiranda27 <d.santosm27@gmail.com>
2023-03-03 19:08:09 -0300

    First commit

---
+++ src/main/java/com/example/demo/model/repository/ClienteRepository.java
@@ -0,0 +1,8 @@
+package com.example.demo.model.repository;
+
+import com.example.demo.model.entity.Cliente;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
+
+}
---
+++ src/main/java/com/example/demo/util/BigDecimalConverter.java
@@ -0,0 +1,17 @@
+package com.example.demo.util;
+
+import org.springframework.stereotype.Component;
+
+import java.math.BigDecimal;
+
+@Component
+public class BigDecimalConverter {
+
+    public BigDecimal converter(String valor) {
+        if (valor == null) {
+            return null;
+        }
+        valor = valor.replace(".", "").replace(",", ".");
+        return new BigDecimal(valor);
+    }
+}
